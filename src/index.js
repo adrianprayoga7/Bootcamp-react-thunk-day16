fd6207b450029c3e0b70e6b556797fea7448f6eb
Andrian Prayoga <adrianprayoga7@gmail.com>
2023-02-21 11:24:55 +0700

    materi input photo from searchbar

--- src/index.js
+++ src/index.js
@@ -1,116 +1,106 @@
+// import { render } from "@testing-library/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import { Comment } from 'semantic-ui-react';
-import { faker } from '@faker-js/faker';
+// import { faker } from '@faker-js/faker';
 import 'semantic-ui-css/semantic.min.css';
+import unsplash from "./unsplash.js";
+import ImageList from "./imageList.js";
 
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
 
-// membuat setiap atribut menggunakan faker
-const data = [
-  {
-  avatar : faker.image.avatar(),
-  name : faker.name.fullName(),
-  time : faker.date.past().toDateString(),
-  // count : faker.datatype.number({min:0, max:100}),
-  comment : faker.lorem.lines()
-},
-{
-  avatar : faker.image.avatar(),
-  name : faker.name.fullName(),
-  time : faker.date.past().toDateString(),
-  // count : faker.datatype.number({min:0, max:100}),
-  comment : faker.lorem.lines()
-}
-];
 
-//kerangka yang akan ditampilkan ke layar menggunakan html
-class CommentContainer extends React.Component {
-  constructor(props) {
-        // agar data yang dirikim dapat diterima
-        super(props);
-        this.state = {
-          count: 0,
-        };
-      }
+
+// class Form extends React.Component {
+//   constructor(props) {
+//   super(props);
+//   this.state = { value: "" };
+  
+//   this.handleChange = this.handleChange.bind(this);
+//   this.handleSubmit = this.handleSubmit.bind(this);
+//   }
+
+//   handleChange(event) {
+//     this.setState({ value: event.target.value });
+//   }
+//   handleSubmit(event) {
+//     alert("A name was submitted: " + this.state.value);
+//     event.preventDefault();
+//   }
+
+//   render() {
+//   return (
+//     <form onSubmit={this.handleSubmit}>
+//       <label>
+//         Name:
+//         <input
+//           type="text"
+//           value={this.state.value}
+//           onChange={this.handleChange}
+//         />
+//       </label>
+//       <input type="submit" value="Submit" />
+//     </form>
+//     );
+//   }
+// }
+
+//untuk membuat search bar dan di tampilkan di layar
+class SearchBar extends React.Component {
+    state = {term: ""};
+
+    onFormSubmit = (event) => {
+      event.preventDefault();
+
+      this.props.onSubmit(this.state.term);
+    };
+
+    render() {
+      return (
+        <div className="ui segment">
+          <form onSubmit={this.onFormSubmit} className="ui form">
+            <div className="field">
+              <label>Image Search</label>
+              <input 
+                type="text"
+                value={this.state.term}
+                onChange={(e) => this.setState({ term: e.target.value})}
+                />
+            </div>
+          </form>
+        </div>
+      );
+    }
+}
+
+//untuk memanggil setiap function atau class untuk dieksekusi
+class App extends React.Component {
+  state = {images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get("/search/photos", {
+      params: {query: term},
+    });
+
+    this.setState({ images: response.data.results});
+    console.log(response.data.results);
+  };
+  //untuk mengeksekusi setiap class
   render() {
     return (
-      <div className="ui container comments">
-        <div className="comment">
-          <a href="/" className="avatar">
-              <img alt="avatar" src={this.props.avatar}></img>
-          </a>
-          <div className="content">
-              <a href="/" className="author">
-                {this.props.name}
-              </a>
-              <div className="metadata">
-                  <span className="date">
-                      {this.props.day} at {this.props.time} | Liked: {this.state.count}
-                  </span>
-              </div>
-              
-              <div className="text">{this.props.comment}</div>
-          </div>
-          <br></br>
-          <button onClick={()=> this.setState({ count: this.state.count + 1})}>Like!</button>
-        </div>
-        <br></br>
+      <div className="ui container" style={{marginTop: "10px"}}>
+          <SearchBar onSubmit={this.onSearchSubmit} />
+          <span>Found: {this.state.images.length} images</span>
+          <ImageList foundImages={this.state.images} />
       </div>
     );
   }
 }
 
-//menginisialisasi data array menggunakan map
-class Comments extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      count: 0,
-    };
-  }
-  render() {
-    return this.props.data.map((dataComment, index) => (
-      <div className="commentContainer" key={index}>
-        {/* pemanggilan atribut */}
-        <CommentContainer
-          avatar={dataComment.avatar}
-          name={dataComment.name}
-          // liked={dataComment.liked}
-          time={dataComment.time}
-          comment={dataComment.comment}
-        />
-      </div>
-    ))
-  }
-}
 
-// class Counting extends React.Component {
-//   constructor(props) {
-//     // agar data yang dirikim dapat diterima
-//     super(props);
-//     this.state = {
-//       count: 0,
-//     };
-//   }
-//   render() {
-//     return (
-//     <div>
-//         <h1>you clicked {this.state.count} times</h1>
-//         <button onClick={()=> this.setState({ count: this.state.count + 1})}>Click on me!</button>
-//     </div>
-//     );
-//   }
-// }
-
-
-// memanggil fungsi co  mment dan data
-const App = () => {
-  return <div>{<Comments data = {data} />}</div>
-}
 
 
 
